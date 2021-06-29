8bbf3dacfad7c5f9345578b49c1e8b2d2322948d
Andrea Lorenzetti <andrea.lorenzetti@nozominetworks.com>
2021-06-29 12:17:26 +0200

    Git init

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+sudo -u developer sed -i -e 's/^SSH_AUTH_SOCK/# SSH_AUTH_SOCK/' /home/developer/.ssh/environment
+
+sudo -u developer git config --global user.name "Andrea Lorenzetti"
+sudo -u developer git config --global user.email "andrea.lorenzetti@nozominetworks.com"
+
+# sudo -u developer cp -rf id_rsa.pub /home/developer/.ssh/id_rsa.pub

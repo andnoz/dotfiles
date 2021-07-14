3918570e5b59911c000410a9f63a19fc916290b0
Andrea Lorenzetti <andrea.lorenzetti@nozominetworks.com>
2021-07-14 14:54:36 +0200

    Removed git conf as `Git commits will be authored using your Coder account details:`

--- install.sh
+++ install.sh
@@ -11,9 +11,6 @@ set -x
 
 sudo -u "$USER_NAME" sed -i -e 's/^SSH_AUTH_SOCK/# SSH_AUTH_SOCK/' "${USER_HOME}/.ssh/environment"
 
-# sudo -u "$USER_NAME" git config --global user.name "Andrea Lorenzetti"
-# sudo -u "$USER_NAME" git config --global user.email "andrea.lorenzetti@nozominetworks.com"
-
 set +x
 
 echo "-- Done executing dotfiles install.sh"

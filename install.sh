38253205390ebb9b5fe87272be80ae030805cb87
Andrea Lorenzetti <andrea.lorenzetti@nozominetworks.com>
2021-07-14 14:24:45 +0200

    Removed git conf

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ set -x
 
 sudo -u "$USER_NAME" sed -i -e 's/^SSH_AUTH_SOCK/# SSH_AUTH_SOCK/' "${USER_HOME}/.ssh/environment"
 
-sudo -u "$USER_NAME" git config --global user.name "Andrea Lorenzetti"
-sudo -u "$USER_NAME" git config --global user.email "andrea.lorenzetti@nozominetworks.com"
+# sudo -u "$USER_NAME" git config --global user.name "Andrea Lorenzetti"
+# sudo -u "$USER_NAME" git config --global user.email "andrea.lorenzetti@nozominetworks.com"
 
 set +x
 
